f3389b93d231a7d82917b35019d24e72df0a1bb0
Ka-Ri D Walker <dywayne.walker@gmail.com>
2019-05-31 17:39:37 -0500

    making it better

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -21,8 +21,8 @@ function buildMetadata(sample) {
 function buildGauge(sample) {
   Plotly.d3.json(`/wfreq/${sample}`, function(error, wfreq) {
     if (error) return console.warn(error);
-    // var level = data.WFREQ;
-    var level = parseFloat(wfreq) * 20;
+    var level = data.WFREQ;
+    // var level = parseFloat(wfreq) * 20;
     // Trig for meter points
     var degrees = 180 - (level*20),
     radius = .7;
